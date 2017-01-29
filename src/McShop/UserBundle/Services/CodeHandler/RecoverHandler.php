0d2bfaa2bb8f46b368be103c2ecbb0b4a8ee630e
Kvintiljanov Aleksandr <bednyj.mops@gmail.com>
2017-01-29 23:17:09 +0300

    - add empty recover handler

---
+++ src/McShop/UserBundle/Services/CodeHandler/RecoverHandler.php
@@ -0,0 +1,18 @@
+<?php
+namespace McShop\UserBundle\Services\CodeHandler;
+
+
+use McShop\UserBundle\Entity\Token;
+
+class RecoverHandler extends AbstractHandler
+{
+
+    /**
+     * @param Token $token
+     * @return bool
+     */
+    public function handle(Token $token)
+    {
+        return true;
+    }
+}
