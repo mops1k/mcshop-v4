500b4f113c9d4af74c193b32ae39b83d7d72dacb
Kvintiljanov Aleksandr <bednyj.mops@gmail.com>
2017-02-05 02:10:44 +0300

    - add server entity

---
+++ app/Resources/Migrations/Version20170204230708.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170204230708 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE server (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, host VARCHAR(255) NOT NULL, port INT NOT NULL, shopping_cart_id VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_5A6DD5F645F80CD (shopping_cart_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE server');
+    }
+}
---
+++ src/McShop/ServersBundle/Entity/Server.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace McShop\ServersBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Server
+ *
+ * @ORM\Table(name="server")
+ * @ORM\Entity(repositoryClass="McShop\ServersBundle\Repository\ServerRepository")
+ */
+class Server
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="host", type="string", length=255)
+     */
+    private $host;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="port", type="integer")
+     */
+    private $port;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="shopping_cart_id", type="string", length=255, unique=true)
+     */
+    private $shoppingCartId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set host
+     *
+     * @param string $host
+     *
+     * @return Server
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+
+        return $this;
+    }
+
+    /**
+     * Get host
+     *
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * Set port
+     *
+     * @param integer $port
+     *
+     * @return Server
+     */
+    public function setPort($port)
+    {
+        $this->port = $port;
+
+        return $this;
+    }
+
+    /**
+     * Get port
+     *
+     * @return int
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Server
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set shoppingCartId
+     *
+     * @param string $shoppingCartId
+     *
+     * @return Server
+     */
+    public function setShoppingCartId($shoppingCartId)
+    {
+        $this->shoppingCartId = $shoppingCartId;
+
+        return $this;
+    }
+
+    /**
+     * Get shoppingCartId
+     *
+     * @return string
+     */
+    public function getShoppingCartId()
+    {
+        return $this->shoppingCartId;
+    }
+}
+
