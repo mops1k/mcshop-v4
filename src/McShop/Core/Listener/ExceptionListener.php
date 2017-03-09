b9a63cf9087e848c6653f8570659538b463fbfa1
mops1k <bednyj.mops@gmail.com>
2017-03-09 13:00:34 +0300

    - add custom theming error page

---
+++ src/McShop/Core/Listener/ExceptionListener.php
@@ -0,0 +1,49 @@
+<?php
+namespace McShop\Core\Listener;
+
+use McShop\Core\Twig\Title;
+use McShop\SettingBundle\Service\SettingHelper;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class ExceptionListener
+{
+    /** @var \Twig_Environment */
+    private $twig;
+    /** @var SettingHelper */
+    private $setting;
+    /** @var Title */
+    private $title;
+    /** @var string */
+    private $env;
+
+    /**
+     * ExceptionListener constructor.
+     * @param \Twig_Environment $twig
+     * @param SettingHelper $setting
+     */
+    public function __construct(\Twig_Environment $twig, SettingHelper $setting, Title $title, $env)
+    {
+        $this->twig = $twig;
+        $this->setting = $setting;
+        $this->title = $title;
+        $this->env = $env;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        if ($this->env === 'dev') {
+            return;
+        }
+
+        $this->title->setValue('system.error')->setAttributes(['@code@' => $event->getException()->getCode() ]);
+
+        $response = new Response(
+            $this->twig->render(':' . $this->setting->get('template', 'Default') . ':errors.html.twig', [
+                'exception' => $event->getException(),
+            ])
+        );
+
+        $event->setResponse($response);
+    }
+}
